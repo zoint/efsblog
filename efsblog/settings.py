5600aa269498500b8706d5df4adf5849e9e2b2d4
jmshapland <jmshapland@gmail.com>
2017-07-25 20:54:29 -0500

    Added ability to show total results for stocks and overall portfolio. Also added humanize to make formatting easier to read.

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'mathfilters',
-    'portfolio'
+    'portfolio',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
--- portfolio/views.py
+++ portfolio/views.py
@@ -169,11 +169,22 @@ def portfolio(request,pk):
    sum_acquired_value = Investment.objects.filter(customer=pk).aggregate(Sum('acquired_value'))
    sum_recent_value = Investment.objects.filter(customer=pk).aggregate(Sum('recent_value')) 
 
+   # Intialize the value of the stocks
+   sum_current_stocks_value = 0
+   sum_of_initial_stock_value = 0
+
+   # Loop through each stock and add the value to the total
+   for stock in stocks:
+       sum_current_stocks_value += stock.current_stock_value()
+       sum_of_initial_stock_value += stock.initial_stock_value()     
+   
+
    return render(request, 'portfolio/portfolio.html', {'customers': customers, 'investments': investments,
                                                       'stocks': stocks,
                                                       'sum_acquired_value': sum_acquired_value,
-                                                      'sum_recent_value': sum_recent_value})
-
+                                                      'sum_recent_value': sum_recent_value,
+                                                      'sum_current_stocks_value': sum_current_stocks_value,
+                                                      'sum_of_initial_stock_value': sum_of_initial_stock_value})
 
 # List at the end of the views.py
 # Lists all customers 
